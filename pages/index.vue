d1159659a18c329462be77b565a0d3797a56546b
Ehsan <xdehsanxd@gmail.com>
2024-10-06 22:17:36 +0330

    Edit background size

--- pages/index.vue
+++ pages/index.vue
@@ -46,11 +46,9 @@ async function handleWeather() {
         :src="`/${background}.jpg`"
         :key="background"
         quality="50"
-        width="100%"
-        height="100%"
         fit="cover"
         loading="eager"
-        class="absolute -z-10"
+        class="absolute -z-10 w-full h-svh"
       />
     </Transition>
     <header class="text-center py-5">
